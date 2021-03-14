418a15677551b73014484563f0fd9883488eddd9
ed.lin <ed.lin@esunergy.com>
2021-03-14 09:50:51 +0800

    新增Articles component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
