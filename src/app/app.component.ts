133c144210452127e116701e31ebb25a24c202b9
ed.lin <ed.lin@esunergy.com>
2021-03-14 10:08:07 +0800

    任務03:使用內嵌繫結顯示title subtitle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo0314';
+  title = 'conduit';
+  subtitle='A place to share your knowledge.';
 }
