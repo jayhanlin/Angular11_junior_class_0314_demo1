24191c16d732e642b0699ad9b26167a0bc993576
ed.lin <ed.lin@esunergy.com>
2021-03-14 10:28:07 +0800

    任務04:講師版使用屬性繫結顯示資料

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conduit';
-  subtitle='A place to share your ';
-  knowledge='knowledge.'
+  subtitle='A place to share your <u>knowledge.</u> ';
 }
